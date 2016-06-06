0daec00dcce13e6bfc95e2c9e5b6ecb11e407efb
Hanush <hanushh@gmail.com>
2016-06-06 22:14:48 +0530

    changes

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -39,13 +39,13 @@
         },
         processCSV: function () {
             var data = Papa.parse(_this.el.csvTA.val(), {delimiter: ","});
-            //console.log(data);
+
 
             _this.el.dataTable
                     .find("thead").html(_this.parseTableHeader(data['data']))
                     .end()
                     .find("tbody").html(_this.parseTableContent(data['data']));
-            
+
             _this.addMarkers(data['data']);
             _this.el.textareaContainer.slideUp(function () {
                 _this.el.dataTableContainer.slideDown('fast');
@@ -65,44 +65,56 @@
         parseTableContent: function (data) {
             var content = document.createDocumentFragment();
             data.shift();
+            _this.clearMarkers();
             data.forEach(function (row) {
-                var tr = $("<tr/>");
+                var tr = $("<tr/>"),
+                        marker = _this.addMarker(row),
+                        infowindow = new google.maps.InfoWindow({
+                            content: _this.generateInfoWindowContentString(row)
+                        });
+                        
+                // Generate <tr/> data
                 row.forEach(function (column) {
-                    
+
                     var td = $("<td/>").html(_this.parseColumnContent(column));
                     td.appendTo(tr);
                 });
                 content.appendChild(tr[0]);
 
-            });
+                // Infowindow Event Listener
+                marker.addListener('click', function () {
+                    infowindow.open(_this.map, marker);
+                });
 
+            });
+            _this.map.panTo(_this.markers[0].position);
             return content;
 
         },
         parseColumnContent: function (data) {
-            if (data.match(/\.(jpeg|jpg|gif|png)$/) != null){
-                
+            if (data.match(/\.(jpeg|jpg|gif|png)$/) !== null) {
+
                 return $("<img/>").attr("src", data).addClass("img-responsive");
             } else {
                 return data;
             }
+
+        },
+        generateInfoWindowContentString:function(data){
+            
             
         },
-        addMarkers: function (data) {
-            _this.clearMarkers();
-            data.forEach(function (row) {
+        addMarker: function (row) {
 
-                var latLng = new google.maps.LatLng(row[9], row[10]), 
-                marker = new google.maps.Marker({
-                    position: latLng,
-                    map: _this.map,
-                    title: row[1]
-                });
-                _this.markers.push(marker);
-            });
-            _this.map.panTo(_this.markers[0].position);
+            var latLng = new google.maps.LatLng(row[9], row[10]),
+                    marker = new google.maps.Marker({
+                        position: latLng,
+                        map: _this.map,
+                        title: row[1]
+                    });
+            _this.markers.push(marker);
+            return marker;
         },
-        
         tableBackBtn: function () {
 
             _this.el.dataTableContainer.slideUp(function () {
@@ -111,7 +123,6 @@
 
 
         },
-        
         clearMarkers: function () {
 
             while (_this.markers.length) {
